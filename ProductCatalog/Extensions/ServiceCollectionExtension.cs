adc5ae6cf1fd882cbf9629fffbc4639d781159fa
Aung Myint Myat <aungmmyat30599@gmail.com>
2024-10-27 13:30:57 +0630

    Update forntend structure

---
+++ ProductCatalog/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Repositories;
+using ProductCatalogCore.Interfaces;
+using ProductCatalogInfrastructure.Data;
+using ProductCatalogInfrastructure.Services;
+
+namespace ProductCatalog.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddCoreScopedConfig(this IServiceCollection services)
+        {
+            // Add Helper
+            _ = services.AddScoped<IRandomizer, Randomizer>();
+            _ = services.AddScoped<ICryptography, Cryptography>();
+            _ = services.AddScoped<CategoryRepo>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddDbAndIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Project DbContext -- SQL Server Connection
+            _ = services.AddDbContext<ProductCatalogContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")).EnableSensitiveDataLogging(), ServiceLifetime.Scoped);
+
+            // Identity DbContext -- SQL Server Connection
+            _ = services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // For Identity User and Role Purpose
+            _ = services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        public static IServiceCollection AddMiscConfig(this IServiceCollection services)
+        {
+            // Add Miscellaneous
+            _ = services.AddHttpContextAccessor();
+
+            // API Lower Case Url
+            _ = services.AddRouting(options => options.LowercaseUrls = true);
+
+            return services;
+        }
+    }
+}
--- ProductCatalog/wwwroot/js/site.js
+++ ProductCatalog/wwwroot/js/site.js
@@ -1,4 +1,68 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function menu_dashboard() {
+    // Menu Item Active Method
+    $('#menu_dashboard').addClass("active");
+}
 
-// Write your JavaScript code.
+function menu_master_category() {
+    // Menu Item Active Method
+    $('#menu_master_collapse').addClass("menu-open");
+
+    // Menu Item Active Method
+    $('#menu_master').addClass("active");
+
+    // Menu Item Extract Collapse
+    $('#menu_master_category').addClass("active");
+}
+
+function numberWithCommas(x) {
+    if (x === null || x === undefined) {
+        return '0';
+    }
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getDateFormat(data) {
+    if (data === null || data === undefined) {
+        return '';
+    }
+    return data.substring(8, 10) + '/' + getMonthName(data.substring(5, 7)) + '/' + data.substring(0, 4);
+}
+
+function getMonthName(no) {
+    if (no == '01') {
+        return "Jan";
+    }
+    else if (no == '02') {
+        return 'Feb';
+    }
+    else if (no == '03') {
+        return 'Mar';
+    }
+    else if (no == '04') {
+        return 'Apr';
+    }
+    else if (no == '05') {
+        return 'May';
+    }
+    else if (no == '06') {
+        return 'Jun';
+    }
+    else if (no == '07') {
+        return 'Jul';
+    }
+    else if (no == '08') {
+        return 'Aug';
+    }
+    else if (no == '09') {
+        return 'Sep';
+    }
+    else if (no == '10') {
+        return 'Oct';
+    }
+    else if (no == '11') {
+        return 'Nov';
+    }
+    else if (no == '12') {
+        return 'Dec';
+    }
+}
