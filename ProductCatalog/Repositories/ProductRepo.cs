f43ffee1a996554ebcab15f2a4e37002454653b5
Aung Myint Myat <aungmmyat30599@gmail.com>
2024-10-28 01:01:39 +0630

    Create ProductRepo.cs

---
+++ ProductCatalog/Repositories/ProductRepo.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using ProductCatalogCore.Entities;
+
+namespace ProductCatalog.Repositories
+{
+    public class ProductRepo
+    {
+        private readonly string? _connectionString;
+
+        public ProductRepo(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<ViProduct>> GetAllProduct()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            try
+            {
+                var result = await connection.QueryAsync<ViProduct>("SELECT * FROM VI_Product WHERE DeletedDate IS NULL");
+                return result.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while retrieving products.", ex);
+            }
+        }
+    }
+}
