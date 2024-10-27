ef9d9ec3c945e2b2d1a2345ea3e232693d400662
Aung Myint Myat <aungmmyat30599@gmail.com>
2024-10-28 01:02:35 +0630

    Update Create.cshtml.cs

--- ProductCatalog/Areas/Master/Pages/Category/Create.cshtml.cs
+++ ProductCatalog/Areas/Master/Pages/Category/Create.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProductCatalog.Repositories;
 
 namespace ProductCatalog.Areas.Master.Pages.Category
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly CategoryRepo? _repository;
+
+        public CreateModel(CategoryRepo repository)
         {
+            _repository = repository;
+        }
+
+        [BindProperty]
+        public ProductCatalogCore.Entities.Category? Category { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Category!.CreatedDate = DateTime.Now;
+            Category.CreatedUser = "Admin";
+            await _repository!.AddCategory(Category);
+
+            return RedirectToPage("./Index");
         }
     }
 }
