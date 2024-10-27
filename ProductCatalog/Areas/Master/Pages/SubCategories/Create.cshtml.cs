c82ba17421141ab5db8e7708527abd43c138d62c
Aung Myint Myat <aungmmyat30599@gmail.com>
2024-10-28 01:02:55 +0630

    Create Create.cshtml.cs

---
+++ ProductCatalog/Areas/Master/Pages/SubCategories/Create.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProductCatalog.Repositories;
+using ProductCatalogCore.Entities;
+
+namespace ProductCatalog.Areas.Master.Pages.SubCategories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SubCategoryRepo? _repository;
+
+        public CreateModel(SubCategoryRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [BindProperty]
+        public SubCategory? SubCategory { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            SubCategory!.CreatedDate = DateTime.Now;
+            SubCategory.CreatedUser = "Admin";
+            await _repository!.AddSubCategory(SubCategory);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
