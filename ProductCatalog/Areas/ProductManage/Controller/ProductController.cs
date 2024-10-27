01e8c316951ba94e227bad0de2e6c007713f1d12
Aung Myint Myat <aungmmyat30599@gmail.com>
2024-10-28 01:03:17 +0630

    Create ProductController.cs

---
+++ ProductCatalog/Areas/ProductManage/Controller/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Model;
+using ProductCatalog.Repositories;
+using ProductCatalogCore.Entities;
+
+namespace ProductCatalog.Areas.ProductManage.Controller
+{
+    [Route("api/productmanage/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepo _repository;
+
+        public ProductController(ProductRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ViProduct>> GetCategoriesAsync()
+        {
+            List<ViProduct> product = await _repository.GetAllProduct();
+            return Ok(new APIRequestModel()
+            {
+                Meta = new { total_count = product.Count },
+                Data = product,
+                Errors = null,
+                Links = null
+            });
+        }
+    }
+}
