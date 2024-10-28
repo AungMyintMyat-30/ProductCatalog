f80b769b1ac9c090c4660bbd0802b85f5cda65cb
Aung Myint Myat <61227624+AungMyintMyat-30@users.noreply.github.com>
2024-10-28 16:12:27 +0630

    Add image service

---
+++ ProductCatalogCore/Interfaces/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalogCore.Interfaces
+{
+    public interface IImageService
+    {
+        public bool Upload(IFormFile file, string name, string folder);
+    }
+}
---
+++ ProductCatalogInfrastructure/Services/ImageService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using ProductCatalogCore.Interfaces;
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ProductCatalogInfrastructure.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IAppLogger<ImageService> _logger;
+        public ImageService(IAppLogger<ImageService> logger)
+        {
+            _logger = logger;
+        }
+        public bool Upload(IFormFile file, string name, string folder)
+        {
+            try
+            {
+                #region Path Built
+
+                string pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\images\\{folder}\\");
+
+                if (!Directory.Exists(pathBuilt))
+                {
+                    Directory.CreateDirectory(pathBuilt);
+                }
+
+                #endregion
+
+                string extension = ("." + file.FileName.Split('.')[^1]).ToLower();
+                string path = pathBuilt + name + extension;
+
+                FileDelete(path);
+                Stream stream = file.OpenReadStream();
+
+                using Bitmap bmpImage = new(stream);
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                Encoder QualityEncoder = Encoder.Quality;
+
+                EncoderParameters myEncoderParameters = new(1);
+
+                EncoderParameter myEncoderParameter = new(QualityEncoder, 50L);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                bmpImage.Save(path, jpgEncoder, myEncoderParameters);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return false;
+            }
+      
+        }
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+        private static bool FileDelete(string path)
+        {
+            FileInfo file = new(path);
+            if (file.Exists) //check file exsit or not 
+            {
+                file.Delete();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
