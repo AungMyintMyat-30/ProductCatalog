47fa3d44700eb0b8c3816904c5e490447cc163e1
Aung Myint Myat <61227624+AungMyintMyat-30@users.noreply.github.com>
2024-10-28 16:46:18 +0630

    Update Index page

--- ProductCatalog/Pages/Index.cshtml.cs
+++ ProductCatalog/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Protocol.Core.Types;
+using ProductCatalog.Repositories;
+using ProductCatalogCore.Entities;
 
 namespace ProductCatalog.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ProductRepo? _repository;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ProductRepo repository, ILogger<IndexModel> logger)
         {
+            _repository = repository;
             _logger = logger;
         }
 
-        public void OnGet()
+        public List<ViProduct>? Product { get; set; }
+        public async Task<IActionResult> OnGetAsync()
         {
+            Product = await _repository!.GetAllProduct();
 
+            return Page();
         }
     }
 }
