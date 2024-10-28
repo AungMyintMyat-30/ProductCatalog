ab574e675bd535d284a4d7421dcfb5ae4e033f37
Aung Myint Myat <61227624+AungMyintMyat-30@users.noreply.github.com>
2024-10-28 16:13:00 +0630

    Update ProductRepo.cs

--- ProductCatalog/Repositories/ProductRepo.cs
+++ ProductCatalog/Repositories/ProductRepo.cs
@@ -7,10 +7,12 @@ namespace ProductCatalog.Repositories
     public class ProductRepo
     {
         private readonly string? _connectionString;
+        private readonly ILogger<ProductRepo> _logger;
 
-        public ProductRepo(IConfiguration configuration)
+        public ProductRepo(IConfiguration configuration, ILogger<ProductRepo> logger)
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection");
+            _logger = logger;
         }
 
         public async Task<List<ViProduct>> GetAllProduct()
@@ -23,6 +25,26 @@ namespace ProductCatalog.Repositories
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
+                throw new Exception("An error occurred while retrieving products.", ex);
+            }
+        }
+
+        public async Task<bool> AddProduct(Product product)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            try
+            {
+                string sql = @"INSERT INTO Product (ProductId, SubId, BrandId, Code, ProductName, Price, Description, ImgUrl, CreatedDate, CreatedUser)
+                       VALUES (@ProductId, @SubId, @BrandId, @Code, @ProductName, @Price, @Description, @ImgUrl, @CreatedDate, @CreatedUser)";
+
+                int rowsAffected = await connection.ExecuteAsync(sql, product);
+
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
                 throw new Exception("An error occurred while retrieving products.", ex);
             }
         }
